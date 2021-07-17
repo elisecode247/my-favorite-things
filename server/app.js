0d50f513f5899301edbc2d1eb9a0f754adacd127
elisecode247 <elisecode247@gmail.com>
2021-07-16 20:11:55 -0700

    save new favorite to json file

--- server/app.js
+++ server/app.js
@@ -1,28 +1,34 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const data = require('../favs.json');
+const data = require('./favs.json');
+const fs = require('fs');
 
-app.use(express.json()); // for parsing application/json
+app.use(express.json());
 
 app.post('/api/v1/favorites', (req, res) => {
-  // get the post request body
-  // add validation
-  // add it to the favs file
   if (!req.body.data) {
     return res.json({ success: false });
   }
-  return res.json({ success: true });
+  const updatedData = data;
+  updatedData.favorites.push(req.body.data);
+  fs.writeFile('./server/favs.json', JSON.stringify(updatedData), err => {
+    if (err) throw err;
+    console.log('Received a post fetch request for a new favorite');
+    res.json({ success: true });
+  });
 });
 
 app.get('/api/v1/favorites', (req, res) => {
+  console.log('Received a get fetch request for the list of favorites');
   res.json({ data: data.favorites });
 });
 
 app.get('/', (req, res) => {
+  console.log('Received a get request for the main page');
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
